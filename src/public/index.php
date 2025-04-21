0796ac83118065b653c97f008980a96f596491f0
Camila Landucci <164803064+Landuurra@users.noreply.github.com>
2025-04-21 00:50:06 -0300

    Update index.php

--- src/public/index.php
+++ src/public/index.php
@@ -16,6 +16,7 @@ $app->addBodyParsingMiddleware();
 
 (require __DIR__ . '/../routes/usuario.php')($app);
 (require __DIR__ . '/../routes/partida.php')($app);
+(require __DIR__ . '/../routes/jugada.php')($app);
 (require __DIR__ . '/../routes/mazo.php')($app);
 
 
