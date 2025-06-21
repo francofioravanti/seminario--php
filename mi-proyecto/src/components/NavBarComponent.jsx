0a5f8486518207fe439b34e59584a3920cfe8731
franco <francofioravanti45@gmail.com>
2025-06-21 16:43:19 -0300

    APLICAMOS EL MENU HAMBURGUESA Y CAMBIAMOS ESTILOS

--- mi-proyecto/src/components/NavBarComponent.jsx
+++ mi-proyecto/src/components/NavBarComponent.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import { useState } from 'react';
 import './NavBarComponent.css';
 
 function NavBarComponent({ isLoggedIn, username, onLogout }) {
   const location = useLocation();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
 
   return (
     <nav className="nav-right">
@@ -16,12 +26,27 @@ function NavBarComponent({ isLoggedIn, username, onLogout }) {
         </>
       ) : (
         <>
-          <div className="nav-left-section">
-            <span className="welcome-text">Hola, {username}!</span>
-            <Link to="/mis-mazos" className="nav-button">Mis Mazos</Link>
-            <Link to="/editar-usuario" className="nav-button">Editar</Link>
+          <span className="welcome-text">Hola, {username}!</span>
+          <div className="menu-container">
+            <button className="hamburger-btn" onClick={toggleMenu}>
+              <span className="hamburger-line"></span>
+              <span className="hamburger-line"></span>
+              <span className="hamburger-line"></span>
+            </button>
+            {isMenuOpen && (
+              <div className="dropdown-menu">
+                <Link to="/mis-mazos" className="dropdown-item" onClick={closeMenu}>
+                  Mazos
+                </Link>
+                <Link to="/editar-usuario" className="dropdown-item " onClick={closeMenu}>
+                  Editar Usuario
+                </Link>
+                <button onClick={() => { onLogout(); closeMenu(); }} className="dropdown-item " >
+                  Cerrar sesión
+                </button>
+              </div>
+            )}
           </div>
-          <button onClick={onLogout} className="nav-button logout-btn">Cerrar sesión</button>
         </>
       )}
     </nav>
