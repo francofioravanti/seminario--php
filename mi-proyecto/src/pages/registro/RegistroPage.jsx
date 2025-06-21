317b7d2386474ab48f94efec1d70ea8600ae3ef5
franco <francofioravanti45@gmail.com>
2025-06-21 17:10:34 -0300

    ARREGLAMOS FOOTER EN REGISTRO

--- mi-proyecto/src/pages/registro/RegistroPage.jsx
+++ mi-proyecto/src/pages/registro/RegistroPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './RegistroPage.css';
-
+import FooterComponent from '../../components/FooterComponent';
 function RegistroPage() {
   const [usuario, setUsuario] = useState('');
   const [nombre, setNombre] = useState('');
@@ -109,6 +109,7 @@ function RegistroPage() {
           </div>
         </div>
       </div>
+      <FooterComponent />
     </div>
   );
 }
