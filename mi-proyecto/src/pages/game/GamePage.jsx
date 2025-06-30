46df21ca42ed4aea1140cb255c2a2589d5648a80
franco <francofioravanti45@gmail.com>
2025-06-30 19:46:00 -0300

    Revertir hasta commit 2

--- mi-proyecto/src/pages/game/GamePage.jsx
+++ mi-proyecto/src/pages/game/GamePage.jsx
@@ -188,11 +188,8 @@ const GamePage = () => {
       <p>Ronda {gameState.currentRound} de {mazoInfo?.cartas.length}</p>
 
       <div className="server-cards">
-        {gameState.serverCards.map((card, idx) => (
-          <div key={idx} className="server-card">
-            <img src={cartaServidorDorso} alt="Carta del servidor" className="server-card-back" />
-            <div className="card-attribute">{card.atributo}</div>
-          </div>
+        {gameState.userCards.map((_, idx) => (
+          <img key={idx} src={cartaServidorDorso} alt="Carta del servidor" className="server-card-back" />
         ))}
       </div>
 
